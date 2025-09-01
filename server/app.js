74f9757ab85ee9da70b5c493db16206e3e846a4d
dillandatanerd <dillanpatelnz@gmail.com>
2025-09-01 17:08:13 +0200

    feat: added students and cohorts routes

--- server/app.js
+++ server/app.js
@@ -5,8 +5,9 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
 
+const cohortData = require("./cohorts.json") 
+const studentData = require("./students.json") 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -28,6 +29,14 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts",(req,res) => {
+  res.json(cohortData)
+})
+
+app.get("/api/students",(req,res)=>{
+  res.json(studentData)
+})
 
 
 // START SERVER
